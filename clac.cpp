07b830caa22a56db253dcb9ee40eaafa578764bd
Thrisha <thrisha122006@gmail.com>
2026-07-19 17:25:58 +0000

    Calculator1

---
+++ clac.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1,n2;
+	cout<<"Enter num1 and num2:";
+	cin>>n1>>n2;
+	cout<<"Enter op";
+	char op;
+	cin>>op;
+	switch(op)
+	{
+		case '+':
+			cout<<"Sum = "<<n1+n2; break;
+		case '-':
+			cout<<"Difference = "<<n1-n2; break;
+		default:
+			cout<<"Invalid choice"; break;
+	}
+}
